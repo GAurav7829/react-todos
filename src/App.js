b53f0b19c3e18d8963982e376554a19acef210f4
Gaurav Singh <grvsgh7@gmail.com>
2024-05-09 20:50:44 +0530

    add todos dynamically

--- src/App.js
+++ src/App.js
@@ -19,6 +19,19 @@ function App() {
       rowAssigned: "Eric",
     },
   ];
+
+  const addTodo = () => {
+    if (todos.length > 0) {
+      const newTodo = {
+        rowNumber: todos.length + 1,
+        rowDescription: "New Description",
+        rowAssigned: "User 3",
+      };
+      todos.push(newTodo);
+      console.log(todos);
+    }
+  };
+
   return (
     // JSX(Javascript XML) - wrapper allows us to write html in react app
     <div className="mt-5 container">
@@ -26,6 +39,9 @@ function App() {
         <div className="card-header">Your TODO's</div>
         <div className="card-body">
           <TodoTable todos={todos} />
+          <button className="btn btn-primary" onClick={addTodo}>
+            Add new todo
+          </button>
         </div>
       </div>
     </div>
