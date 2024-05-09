d8c841b3c085b53753192b2812fe2dfaf5d7fd55
Gaurav Singh <grvsgh7@gmail.com>
2024-05-09 20:34:38 +0530

    added TodoRowItem and TodoTable component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TodoTable from "./components/TodoTable";
 
 function App() {
+  const todos = [
+    {
+      rowNumber: 1,
+      rowDescription: "Description 1",
+      rowAssigned: "Eric",
+    },
+    {
+      rowNumber: 2,
+      rowDescription: "Description 2",
+      rowAssigned: "Eric",
+    },
+    {
+      rowNumber: 3,
+      rowDescription: "Description 3",
+      rowAssigned: "Eric",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-          
-      </header>
+    // JSX(Javascript XML) - wrapper allows us to write html in react app
+    <div className="mt-5 container">
+      <div className="card">
+        <div className="card-header">Your TODO's</div>
+        <div className="card-body">
+          <TodoTable todos={todos} />
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  //rendering for all react app, App is a component, App is App.js
   <React.StrictMode>
     <App />
   </React.StrictMode>
